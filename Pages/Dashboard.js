6b65fe12a11356415bab6e2f19af19fa0ab970cb
Pragalya Kanakaraj <pragalyakanakaraj@gmail.com>
2024-12-09 14:49:37 +0530

    Updated dsahboard

--- Pages/Dashboard.js
+++ Pages/Dashboard.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Sidebar from '../components/sidebar';
-import UserTable from '../components/UserManagement'
+import UserTable from '../components/Dashboard/UserManagement'
+import TopBar from '../components/topbar'
 const Dashboard = () => {
-  return ( 
-    <div style={{ display: 'flex' }}>
-      <Sidebar>
-        <UserTable/>
-      </Sidebar>
-
-    </div>
+  return (
+    <>
+      <div className=' h-screen'>
+        <TopBar /> {/* Top navbar */}
+        <Sidebar />
+        <div className='w-full md:pl-60 md:pt-20 md:pr-8 p-3 bg-gray-100 h-full '>
+            <UserTable />
+        </div>
+      </div>
+    </>
   );
 }
 
